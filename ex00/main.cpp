a1ecf3fc3df8d025f2c7a359cc8cba9ab9becc5f
cmakario <cmakario@student.42.fr>
2025-05-11 15:16:29 +0200

    added call of database & headers

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,13 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:59:44 by cmakario          #+#    #+#             */
-/*   Updated: 2025/05/08 18:09:02 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/05/11 15:15:47 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "BitcoinExchange.hpp"
+#include <iostream> // std::cerr, std::cout
+
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -20,8 +22,8 @@ int main(int argc, char **argv) {
 	}
 
 	BitcoinExchange btc;
-	if (!btc.loadDB(argv[1])) {
-		std::cerr << "❌ Error: File cannot be opened." << std::endl;
+	if (!btc.loadDatabase("data.csv")) {									// had to be hardcoded...
+		std::cerr << "❌ Error: \"data.csv\" file cannot be opened." << std::endl;
 		return 1;
 	}
 
@@ -30,3 +32,21 @@ int main(int argc, char **argv) {
 	std::cout << "✅ File processed successfully." << std::endl;
 	return 0;
 }
+
+//  if i wanted to use the database file as an argument, i would have to change the code in the main function
+/* int main(int argc, char **argv) {
+    if (argc != 2 && argc != 3) {
+        std::cerr << "Usage: ./btc <input_file> [database_file]" << std::endl;
+        return 1;
+    }
+
+    std::string dbFile = (argc == 3) ? argv[2] : "data.csv";
+    
+    BitcoinExchange btc;
+    if (!btc.loadDatabase(dbFile))
+        return 1;
+
+    btc.processInput(argv[1]);
+    return 0;
+}
+ */
