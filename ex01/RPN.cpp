6fef1cf5f529624a4363c0dfa963a4374711b019
cmakario <cmakario@student.42.fr>
2025-05-13 07:53:57 +0200

    use of istringstream

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,32 +6,91 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 20:19:32 by cmakario          #+#    #+#             */
-/*   Updated: 2025/05/12 13:27:58 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/05/13 07:52:56 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include <sstream> // for std::istringstream
 #include <stdexcept> // for std::invalid_argument and std::runtime_error
+// #include <iostream> // for debugging
 
 
 bool RPN::isOperator(char c) const {
 	return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
+void RPN::applyOperator(char op, std::stack<int> &stack) {
+	if (stack.size() < 2) {
+		throw std::runtime_error("❌ Error: Not enough operands on the stack.");
+	}
+	
+	int right = stack.top();
+	stack.pop();
+	int left = stack.top();
+	stack.pop();
+	int result = 0;
+	
+	switch (op) {
+		case '+':
+			result = left + right;
+			break;
+		case '-':
+			result = left - right;
+			break;
+		case '*':
+			result = left * right;
+			break;
+		case '/':
+			if (right == 0) {
+				throw std::runtime_error("❌ Error: Division by zero.");
+			}
+			result = left / right;
+			break;
+		default:
+			throw std::invalid_argument("❌ Error: Unknown operator");
+		}
+		
+	stack.push(result);
+}
+
+
 // v.1
+// int RPN::evaluate(const std::string &input) {
+// 	std::stack<int> stack;
+// 	for (char c : input) {
+// 		if (std::isspace(c)) {
+// 			continue;
+// 		}
+// 		if (std::isdigit(c)) {
+// 			stack.push(c - '0');
+// 		} else if (isOperator(c)) {
+// 			applyOperator(c, stack);
+// 		} else {
+// 			throw std::invalid_argument("Invalid character in expression");
+// 		}
+// 		// std::cout << "❌ " << stack.size() << std::endl;
+// 	}
+// 	if (stack.size() != 1) {
+// 		// std::cout << "❌ Error: " << stack.size() << std::endl;
+// 		throw std::runtime_error("Invalid RPN expression");
+// 	}
+// 	return stack.top();
+// }
+
+// v.2 with the use of std::istringstream
 int RPN::evaluate(const std::string &input) {
 	std::stack<int> stack;
-	for (char c : input) {
-		if (std::isspace(c)) {
-			continue;
-		}
-		if (std::isdigit(c)) {
-			stack.push(c - '0');
-		} else if (isOperator(c)) {
-			applyOperator(c, stack);
+	std::istringstream iss(input);
+	std::string token;
+
+	while (iss >> token) {
+		if (std::isdigit(token[0])) {
+			stack.push(std::stoi(token));
+		} else if (isOperator(token[0])) {
+			applyOperator(token[0], stack);
 		} else {
-			throw std::invalid_argument("Invalid character in expression");
+			throw std::invalid_argument("Invalid token in expression");
 		}
 	}
 	if (stack.size() != 1) {
@@ -39,4 +98,3 @@ int RPN::evaluate(const std::string &input) {
 	}
 	return stack.top();
 }
-
