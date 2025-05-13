a44c5080a4a090965edabc3c4ca9c6fd2d99fb5a
cmakario <cmakario@student.42.fr>
2025-05-13 10:46:44 +0200

    fix output throws

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,13 +6,14 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 20:19:32 by cmakario          #+#    #+#             */
-/*   Updated: 2025/05/13 07:52:56 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/05/13 10:43:14 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 #include <sstream> // for std::istringstream
 #include <stdexcept> // for std::invalid_argument and std::runtime_error
+#include <cctype> // for std::isdigit
 // #include <iostream> // for debugging
 
 
@@ -56,30 +57,30 @@ void RPN::applyOperator(char op, std::stack<int> &stack) {
 
 
 // v.1
-// int RPN::evaluate(const std::string &input) {
-// 	std::stack<int> stack;
-// 	for (char c : input) {
-// 		if (std::isspace(c)) {
-// 			continue;
-// 		}
-// 		if (std::isdigit(c)) {
-// 			stack.push(c - '0');
-// 		} else if (isOperator(c)) {
-// 			applyOperator(c, stack);
-// 		} else {
-// 			throw std::invalid_argument("Invalid character in expression");
-// 		}
-// 		// std::cout << "❌ " << stack.size() << std::endl;
-// 	}
-// 	if (stack.size() != 1) {
-// 		// std::cout << "❌ Error: " << stack.size() << std::endl;
-// 		throw std::runtime_error("Invalid RPN expression");
-// 	}
-// 	return stack.top();
-// }
+/* int RPN::evaluate(const std::string &input) {
+	std::stack<int> stack;
+	for (char c : input) {
+		if (std::isspace(c)) {
+			continue;
+		}
+		if (std::isdigit(c)) {
+			stack.push(c - '0');
+		} else if (isOperator(c)) {
+			applyOperator(c, stack);
+		} else {
+			throw std::invalid_argument("Invalid character in expression");
+		}
+		// std::cout << "❌ " << stack.size() << std::endl;
+	}
+	if (stack.size() != 1) {
+		// std::cout << "❌ Error: " << stack.size() << std::endl;
+		throw std::runtime_error("Invalid RPN expression");
+	}
+	return stack.top();
+} */
 
 // v.2 with the use of std::istringstream
-int RPN::evaluate(const std::string &input) {
+/* int RPN::evaluate(const std::string &input) {
 	std::stack<int> stack;
 	std::istringstream iss(input);
 	std::string token;
@@ -98,3 +99,33 @@ int RPN::evaluate(const std::string &input) {
 	}
 	return stack.top();
 }
+ */
+
+// v.2.1
+int RPN::evaluate(const std::string &input) {
+	if (input.empty())
+		throw std::invalid_argument("❌ Error: Expression is empty.");
+		
+	std::stack<int> stack;
+	std::istringstream iss(input);
+	std::string token;
+
+	while (iss >> token) {
+		if (token.length() == 1 && std::isdigit(token[0])) {
+			int num = token[0] - '0';
+			stack.push(num);
+		} else if (token.length() == 1 && isOperator(token[0])) {
+				applyOperator(token[0], stack);
+		} else if (std::isdigit(token[0]) && token.length() > 1) {
+			throw std::invalid_argument("❌ Error: Multi-digit numbers are not allowed: \"" + token + "\"");
+		} else {
+			 throw std::invalid_argument("❌ Error: Invalid token: \"" + token + "\"");
+		}
+	}
+
+	if (stack.size() != 1) {
+		throw std::runtime_error("❌ Error: Invalid expression (stack size != 1)");
+	}
+	return stack.top();
+}
+
